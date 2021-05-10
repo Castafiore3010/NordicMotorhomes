b2389649219b5cae543b9ed96c301b9d6b48c137
45304 <marc474j@stud.kea.dk>
2021-05-10 17:01:55 +0200

    Initial commit

---
+++ src/test/java/com/example/nordicmotorhomes/NordicMotorhomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nordicmotorhomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicMotorhomesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
